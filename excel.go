7b2c572c5d06913a596f21b91b4172c5dd51e230
张大鹏 <pygosuperman@outlook.com>
2022-02-07 07:52:23 +0800

    Excel插入图表和图片

--- excel.go
+++ excel.go
@@ -2,24 +2,27 @@ package zdpgo_file
 
 import (
 	"github.com/xuri/excelize/v2"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 )
 
 // 参考文档：https://github.com/qax-os/excelize
 
-// NewExcel 创建Excel文件
-func (f *File) NewExcel(config FileExcelConfig) {
+// ExcelNew 创建Excel文件
+func (f *File) ExcelNew(config FileExcelConfig) {
 	f.excel = excelize.NewFile()
 	f.excelConfig = &config
 }
 
-// NewSheet 创建工作簿
-func (f *File) NewSheet(name string) int {
+// ExcelNewSheet 创建工作簿
+func (f *File) ExcelNewSheet(name string) int {
 	index := f.excel.NewSheet(name)
 	return index
 }
 
-// SetCellValue 设置单元格的值
-func (f *File) SetCellValue(sheet string, cell string, value interface{}) error {
+// ExcelSetCellValue 设置单元格的值
+func (f *File) ExcelSetCellValue(sheet string, cell string, value interface{}) error {
 	err := f.excel.SetCellValue(sheet, cell, value)
 	if err != nil {
 		return err
@@ -27,17 +30,13 @@ func (f *File) SetCellValue(sheet string, cell string, value interface{}) error 
 	return nil
 }
 
-// SetActiveSheet 激活单元格
-func (f *File) SetActiveSheet(sheetIndex int) error {
-	err := f.SetActiveSheet(sheetIndex)
-	if err != nil {
-		return err
-	}
-	return nil
+// ExcelSetActiveSheet 激活单元格
+func (f *File) ExcelSetActiveSheet(sheetIndex int) {
+	f.excel.SetActiveSheet(sheetIndex)
 }
 
-// SaveExcel 保存Excel
-func (f *File) SaveExcel() error {
+// ExcelSave 保存Excel
+func (f *File) ExcelSave() error {
 	if f.excelConfig.FileName == "" {
 		f.log.Warning("Excel文件名为空，将使用默认的文件名：zdpgo_file.xlsx")
 		f.excelConfig.FileName = "zdpgo_file.xlsx"
@@ -95,4 +94,66 @@ func (f *File) GetSheetData(sheetName string) (data [][]string, err error) {
 		f.log.Error("读取Excel单元格失败", "error", err.Error())
 	}
 	return data, err
+}
+
+// ExcelAddChart 添加图表到Excel
+func (f *File) ExcelAddChart(sheetName string, cellName string, jsonData string) (err error) {
+	if f.excelConfig.FileName == "" {
+		f.log.Warning("Excel文件名为空，将使用默认的文件名：zdpgo_file.xlsx")
+		f.excelConfig.FileName = "zdpgo_file.xlsx"
+	}
+
+	// 打开Excel
+	if f.excel == nil {
+		f.excel, err = excelize.OpenFile(f.excelConfig.FileName)
+		if err != nil {
+			f.log.Error("打开Excel失败", "error", err.Error())
+			return err
+		}
+	}
+
+	// 添加图
+	err = f.excel.AddChart(sheetName, cellName, jsonData)
+	if err != nil {
+		f.log.Error("添加图表失败", "error", err.Error())
+	}
+	return err
+}
+
+func (f *File) ExcelAddPicture(sheetName string, cellName string, img string, jsonConfig string) (err error) {
+	if f.excelConfig.FileName == "" {
+		f.log.Warning("Excel文件名为空，将使用默认的文件名：zdpgo_file.xlsx")
+		f.excelConfig.FileName = "zdpgo_file.xlsx"
+	}
+
+	// 打开Excel
+	if f.excel == nil {
+		f.excel, err = excelize.OpenFile(f.excelConfig.FileName)
+		if err != nil {
+			f.log.Error("打开Excel失败", "error", err.Error())
+			return err
+		}
+	}
+
+	// 添加图
+	err = f.excel.AddPicture(sheetName, cellName, img, jsonConfig)
+	f.log.Info("添加图片的配置", "jsonConfig", jsonConfig)
+	if err != nil {
+		f.log.Error("添加图片失败", "error", err.Error())
+	}
+	return err
+}
+
+func (f *File) ExcelClose() (err error) {
+	// excel 没有打开
+	if f.excel == nil {
+		return nil
+	}
+
+	// 关闭
+	err = f.excel.Close()
+	if err != nil {
+		f.log.Error("关闭Excel失败", "error", err.Error())
+	}
+	return err
 }
--- excel_test.go
+++ excel_test.go
@@ -7,27 +7,27 @@ import (
 
 func TestFile_NewExcel(t *testing.T) {
 	f := prepareFile()
-	f.NewExcel(FileExcelConfig{FileName: "test.xlsx"})
-	f.NewSheet("测试")
-	f.SetCellValue("测试", "A1", "姓名")
-	f.SetCellValue("测试", "B1", "性别")
-	f.SetCellValue("测试", "C1", "年龄")
-	f.SetCellValue("测试", "A2", "张三")
-	f.SetCellValue("测试", "B2", "男")
-	f.SetCellValue("测试", "C2", "22")
-	f.SaveExcel()
+	f.ExcelNew(FileExcelConfig{FileName: "test.xlsx"})
+	f.ExcelNewSheet("测试")
+	f.ExcelSetCellValue("测试", "A1", "姓名")
+	f.ExcelSetCellValue("测试", "B1", "性别")
+	f.ExcelSetCellValue("测试", "C1", "年龄")
+	f.ExcelSetCellValue("测试", "A2", "张三")
+	f.ExcelSetCellValue("测试", "B2", "男")
+	f.ExcelSetCellValue("测试", "C2", "22")
+	f.ExcelSave()
 }
 
 func TestFile_GetCellValue(t *testing.T) {
 	f := prepareFile()
-	f.NewExcel(FileExcelConfig{FileName: "test.xlsx"})
+	f.ExcelNew(FileExcelConfig{FileName: "test.xlsx"})
 	value, err := f.GetCellValue("测试", "A1")
 	fmt.Println(value, err)
 }
 
 func TestFile_GetSheetData(t *testing.T) {
 	f := prepareFile()
-	f.NewExcel(FileExcelConfig{FileName: "test.xlsx"})
+	f.ExcelNew(FileExcelConfig{FileName: "test.xlsx"})
 	data, err := f.GetSheetData("测试")
 	fmt.Println(data, err)
 
@@ -37,4 +37,94 @@ func TestFile_GetSheetData(t *testing.T) {
 		}
 		fmt.Println()
 	}
+}
+
+func TestFile_ExcelAddChart(t *testing.T) {
+	f := prepareFile()
+	f.ExcelNew(FileExcelConfig{FileName: "test.xlsx"})
+
+	// 分类
+	categories := map[string]string{
+		"A2": "Small", "A3": "Normal", "A4": "Large",
+		"B1": "Apple", "C1": "Orange", "D1": "Pear"}
+
+	// 值
+	values := map[string]int{
+		"B2": 2, "C2": 3, "D2": 3, "B3": 5, "C3": 2, "D3": 4, "B4": 6, "C4": 7, "D4": 8}
+
+	// 添加分类
+	for k, v := range categories {
+		f.ExcelSetCellValue("Sheet1", k, v)
+	}
+
+	// 添加值
+	for k, v := range values {
+		f.ExcelSetCellValue("Sheet1", k, v)
+	}
+
+	// 添加图表
+	if err := f.ExcelAddChart("Sheet1", "E1", `{
+        "type": "col3DClustered",
+        "series": [
+        {
+            "name": "Sheet1!$A$2",
+            "categories": "Sheet1!$B$1:$D$1",
+            "values": "Sheet1!$B$2:$D$2"
+        },
+        {
+            "name": "Sheet1!$A$3",
+            "categories": "Sheet1!$B$1:$D$1",
+            "values": "Sheet1!$B$3:$D$3"
+        },
+        {
+            "name": "Sheet1!$A$4",
+            "categories": "Sheet1!$B$1:$D$1",
+            "values": "Sheet1!$B$4:$D$4"
+        }],
+        "title":
+        {
+            "name": "Fruit 3D Clustered Column Chart"
+        }
+    }`); err != nil {
+		fmt.Println(err)
+		return
+	}
+	f.ExcelSave()
+}
+
+func TestFile_ExcelAddPicture(t *testing.T) {
+	f := prepareFile()
+	f.ExcelNew(FileExcelConfig{FileName: "test.xlsx"})
+
+	defer func() {
+		if err := f.ExcelClose(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	img := "D:\\data\\image\\wzry\\1.jpg"
+
+	// 插入一张图片
+	if err := f.ExcelAddPicture("Sheet1", "A2", img, ""); err != nil {
+		fmt.Println(err)
+	}
+
+	// 插入一张图片并缩放
+	if err := f.ExcelAddPicture("Sheet1", "D2", img,
+		`{"x_scale": 0.5, "y_scale": 0.5}`); err != nil {
+		fmt.Println(err)
+	}
+
+	// 插入一张图片并支持打印
+	if err := f.ExcelAddPicture("Sheet1", "H2", img, `{
+        "x_offset": 15,
+        "y_offset": 10,
+        "print_obj": true,
+        "lock_aspect_ratio": false,
+        "locked": false
+    }`); err != nil {
+		fmt.Println(err)
+	}
+
+	f.ExcelSave()
 }
